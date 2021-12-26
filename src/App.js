bee34b88da07dd953ed5428cc6e1f32370f260a6
Berlie <berl.yiadom@gmail.com>
2021-12-26 22:47:44 +0000

    Updated the App.js component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <h1>
-         Hello World
-        </h1>
-        
-      </header>
+      <Weather />
     </div>
   );
 }
