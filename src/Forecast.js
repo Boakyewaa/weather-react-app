0666b253d02cb665855d0e93041a5c7f5e1c5a43
Berlie <berl.yiadom@gmail.com>
2021-12-27 09:02:42 +0000

    Created the Forecast.js and updated Weather.js

---
+++ src/Forecast.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default Forecast();{
+    return(
+        <div className="weather-forecast">
+        <div className="row">
+        <div className="col-2">
+        <div>
+        Mon
+        </div>
+        <img src="" alt="icon" />
+        <div>
+        20 19
+        </div>
+        </div>
+        </div>
+        </div>
+    );
+}
