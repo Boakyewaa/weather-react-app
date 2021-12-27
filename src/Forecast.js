5114e9d85966d0fbb62962400411566cbe915b4a
Berlie <berl.yiadom@gmail.com>
2021-12-27 09:26:35 +0000

    Updated App.css and Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,19 +1,70 @@
 import React from "react";
 
-export default Forecast();{
+export default function Forecast(){
     return(
-        <div className="weather-forecast">
-        <div className="row">
-        <div className="col-2">
-        <div>
-        Mon
-        </div>
-        <img src="" alt="icon" />
-        <div>
-        20 19
-        </div>
-        </div>
-        </div>
+         <div className="Weather-Forecast">
+            <div className="row">
+                <div className="col-2">
+                   <div>
+                       Mon</div> 
+                    <img src="" alt="icon"/>
+                   <div>
+                       19 13
+                    </div> 
+                </div>
+            </div>
+            <div className="row" >
+                <div className="col-2">
+                   <div>
+                      Tue</div> 
+                    <img src="" alt="icon"/>
+                   <div>
+                       29 31
+                    </div> 
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2">
+                   <div>
+                       Wed</div> 
+                    <img src="" alt="icon"/>
+                   <div>
+                       28 23
+                    </div> 
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2">
+                   <div>
+                       Thu</div> 
+                    <img src="" alt="icon"/>
+                   <div>
+                       30 33
+                    </div> 
+                </div>
+            </div>
+        <div className="row" >
+                <div className="col-2">
+                   <div>
+                       Fri</div> 
+                    <img src="" alt="icon"/>
+                   <div>
+                       25 23
+                    </div> 
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2">
+                   <div >
+                       Sat</div> 
+                    <img src="" alt="icon"/>
+                   <div>
+                       22 23
+                    </div> 
+                </div>
+            </div>
+
+            
         </div>
     );
 }
